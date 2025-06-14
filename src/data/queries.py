a7e61767fbe5174b988aadb3bdd3baa68f304a42
tom avilius <tomavilius@tomavilius.in>
2025-06-14 16:29:14 +0530

    Add small sample data.

---
+++ src/data/queries.py
@@ -0,0 +1,67 @@
+queries = [
+    {
+        "news": "Virat Kohli became the highest run-scorer in World Cup history in 2023.",
+        "flag": True,
+    },
+    {
+        "news": "Virat Kohli announced retirement before the 2023 World Cup.",
+        "flag": False,
+    },
+    {
+        "news": "India launched the Digital Personal Data Protection Act in 2023.",
+        "flag": True,
+    },
+    {
+        "news": "India legalized data collection without user consent in 2023.",
+        "flag": False,
+    },
+    {
+        "news": "Tesla opened its first manufacturing plant in India in 2024.",
+        "flag": True,
+    },
+    {"news": "Tesla exited the Indian market permanently in 2024.", "flag": False},
+    {"news": "ISRO’s Aditya-L1 mission was launched to study the Sun.", "flag": True},
+    {
+        "news": "ISRO canceled all solar observation missions after Chandrayaan-3.",
+        "flag": False,
+    },
+    {
+        "news": "The Reserve Bank of India launched the Digital Rupee pilot project in 2022.",
+        "flag": True,
+    },
+    {"news": "India banned all forms of digital currency in 2022.", "flag": False},
+    {"news": "Microsoft integrated AI Copilot in Windows 11 in 2023.", "flag": True},
+    {
+        "news": "Microsoft replaced Windows 11 with Linux-based OS in 2023.",
+        "flag": False,
+    },
+    {
+        "news": "India won the Asian Games 2023 gold medal in men’s cricket.",
+        "flag": True,
+    },
+    {"news": "India did not participate in the 2023 Asian Games.", "flag": False},
+    {
+        "news": "The government of India introduced the PM eBus Sewa scheme in 2023.",
+        "flag": True,
+    },
+    {
+        "news": "PM eBus Sewa was scrapped due to lack of funding in 2023.",
+        "flag": False,
+    },
+    {
+        "news": "Google DeepMind released AlphaFold 3 for protein structure prediction in 2024.",
+        "flag": True,
+    },
+    {
+        "news": "Google shut down DeepMind's research on protein folding in 2024.",
+        "flag": False,
+    },
+    {
+        "news": "India’s 2024 interim budget focused on infrastructure and green energy.",
+        "flag": True,
+    },
+    {
+        "news": "India’s 2024 budget introduced cryptocurrency as legal tender.",
+        "flag": False,
+    },
+]
